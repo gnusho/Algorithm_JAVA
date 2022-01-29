817e6d12c1a0f12b4d0f1f21b9f1d41cdfb5ec4d
ohsungjoon <31716977+ohsungjoon@users.noreply.github.com>
2022-01-29 14:06:38 +0900

    백준 1단계 추가

---
+++ baekjoon/StepByStep/src/step1/Boj10926.java
@@ -0,0 +1,13 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Boj10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.next() + "??!");
+	}
+
+}
---
+++ baekjoon/StepByStep/src/step1/Boj18108.java
@@ -0,0 +1,13 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Boj18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextInt() - (2541 - 1998));
+	}
+
+}
