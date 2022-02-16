4b44c2aaf40f424a119b0fcf28e1bdd03b445bf8
ohsungjoon <31716977+ohsungjoon@users.noreply.github.com>
2022-02-16 19:27:18 +0900

    문자열

--- baekjoon/Categories/src/string/Boj16120.java
+++ baekjoon/Categories/src/string/Boj16120.java
@@ -1,15 +1,39 @@
 package string;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Boj16120 {
 
-	static String ppap;
-	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ppap = sc.next();
 		
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		
+		if(ppap(input)) System.out.println("PPAP");
+		else System.out.println("NP");
 	}
 
+	public static boolean ppap(String input) {
+		boolean ret = true;
+		Stack<Integer> st = new Stack<Integer>();
+		
+		for(int i = 0; i < input.length(); i++) {
+			int tmp = input.charAt(i);
+			if(tmp == 'P') st.push(tmp);
+			else if(tmp == 'A'){
+				if(st.size() < 2 || i + 1 >= input.length() || input.charAt(i + 1) != 'P') {
+					ret = false;
+					break;
+				} else {
+					st.pop();
+					i++;
+				}
+			}
+		}
+		
+		if(st.size() > 1) ret = false;
+		
+		return ret;
+	}
 }
