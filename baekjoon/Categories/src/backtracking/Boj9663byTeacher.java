60eec065a81898288ae0d6e6a1ac274695c47bbf
ohsungjoon <31716977+ohsungjoon@users.noreply.github.com>
2022-02-17 11:25:51 +0900

    BackTracking

---
+++ baekjoon/Categories/src/backtracking/Boj9663byTeacher.java
@@ -0,0 +1,39 @@
+package backtracking;
+
+import java.util.Scanner;
+
+public class Boj9663byTeacher {
+
+	static int col[], N, ans;
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		ans = 0;
+		col = new int[N + 1];
+		setQueen(1);
+		System.out.println(ans);
+	}
+	
+	public static void setQueen(int rowNo) {
+		if(!isAvailable(rowNo - 1)) return;
+		
+		if(rowNo > N) {
+			ans++;
+			return;
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			col[rowNo] = i;
+			setQueen(rowNo + 1);
+		}
+	}
+
+	public static boolean isAvailable(int rowNo) {
+		for(int i = 1; i < rowNo; i++) {
+			if(col[rowNo] == col[i] || rowNo - i == Math.abs(col[rowNo] - col[i])) return false;
+		}
+		return true;
+	}
+}
